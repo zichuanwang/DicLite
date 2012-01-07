96e7ef0f4078aa1a0fcb1a2a528f41e4d7c620a9
bluebitch <wzc345@gmail.com>
2012-01-08 01:23:55 +0800

    Initial Commit

---
+++ dict/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  dict
+//
+//  Created by Blue Bitch on 12-1-8.
+//  Copyright (c) 2012年 TJU. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
